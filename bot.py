e3057fb55fd8ddeb87713daa7a902e134589bc6c
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-09-24 21:13:40 -0400

    selenium webdriver

--- bot.py
+++ bot.py
@@ -9,6 +9,17 @@ from itertools import cycle
 load_dotenv()
 from discord.ext import commands, tasks
 
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_exec_shim = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
+opts = webdriver.ChromeOptions()
+opts.binary_location = chrome_exec_shim
+opts.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
 bot = commands.Bot(command_prefix='?', description='SBU HACKS BOT!!')
 TOKEN = os.environ.get('TOKEN', 3)
 
@@ -18,6 +29,10 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(type=discord.ActivityType.watching, name="your data ;)", emoji=party))
     print('bot.py is active')
+
+
+
+
 
 @bot.command(name='hello')
 async def hello(ctx):
@@ -31,6 +46,12 @@ async def join(ctx):
 @bot.command(name="leave")
 async def join(ctx):
     await ctx.voice_client.disconnect()
+
+@bot.command(name="google")
+async def join(ctx, *, search):
+    driver.get("https://google.com")
+
+# ---------------------------------------
 
 @bot.command(hidden=True)
 async def load(ctx):
